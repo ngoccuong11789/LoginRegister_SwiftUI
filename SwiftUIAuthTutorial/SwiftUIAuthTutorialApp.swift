d2696cfe89169926394935ba5833d36a23ff83b2
Cuong Nguyen <ngoccuong11789@gmail.com>
2024-10-29 19:08:47 -0500

    Initial Commit

---
+++ SwiftUIAuthTutorial/SwiftUIAuthTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthTutorialApp.swift
+//  SwiftUIAuthTutorial
+//
+//  Created by Cuong Nguyen Ngoc on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
